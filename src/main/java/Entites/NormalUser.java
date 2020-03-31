3090bf528a57ef83056c6d4381921cfc9acfb3e4
NourhanEl-Khodary <36134694+NourhanEl-khodary@users.noreply.github.com>
2020-04-01 01:33:43 +0200

    UserDAL & related classes + NormalUser Database schema

--- src/main/java/Entites/NormalUser.java
+++ src/main/java/Entites/NormalUser.java
@@ -3,6 +3,11 @@ package Entites;
 public class NormalUser extends User {
     //ShoppingCart shopingCart;
 
+    public NormalUser(String name, String email, String pass, String gender, String birthdate, String mobileNo)
+    {
+        super(name, email, pass, gender, birthdate, mobileNo);
+    }
+
 }
 
 
---
+++ src/main/java/Presistance/UserSQL_DAL.java
@@ -0,0 +1,100 @@
+package Presistance;
+
+import Entites.User;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class UserSQL_DAL extends UserDAL
+{
+    static Connection con;
+    static Statement statement;
+    String url = "jdbc:mysql://localhost:3306/online_shopping";
+    String userName = "root";
+    String password = "123456789";
+
+    UserSQL_DAL()
+    {
+        if(con == null) {
+            try {
+                con = DriverManager.getConnection(url, userName, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean SaveUser(String name, String email, String pass, String gender, String birthdate, String mobileNo, int type) throws ParseException, SQLException
+    {
+        statement = con.createStatement();
+        String query = "INSERT INTO `user_`(`USER_TYPE`, `USER_NAME`, `BDATE`, `USER_PASSWORD`, `EMAIL`, `GENDER`, `MOBILE_NUM`) VALUES (?,?,?,?,?,?,?)";
+
+        PreparedStatement preparedStmt = null;
+        try {
+            preparedStmt = con.prepareStatement(query);
+            preparedStmt.setInt(1, type);
+            preparedStmt.setString(2, name);
+
+            Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(birthdate);
+            preparedStmt.setDate(3,  new java.sql.Date(date1.getTime()));
+
+            preparedStmt.setString(4, pass);
+            preparedStmt.setString(5, email);
+
+            preparedStmt.setString(6, gender);
+            preparedStmt.setString(7, mobileNo);
+
+            preparedStmt.execute();
+
+        } catch (SQLException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<User> LoadUser()
+    {
+
+        ArrayList<User> user_InSystem = new ArrayList<>();
+        User user;
+
+        try
+        {
+            String query = "SELECT * FROM user_ ";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next())
+            {
+                int type = resultSet.getInt(2);
+                String name = resultSet.getString(3);
+                Date date = resultSet.getDate(4);
+                String pass = resultSet.getString(5);
+                String email = resultSet.getString(6);
+                String gender = resultSet.getString(7);
+                String mobileNo = resultSet.getString(8);
+                String d = date.toString();
+
+                user = f.Create(type,name, email, pass, gender, d, mobileNo);
+                user_InSystem.add(user);
+            }
+
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+
+        return user_InSystem;
+    }
+
+    @Override
+    public UserDAL getInstance()
+    {
+        return null;
+    }
+}
